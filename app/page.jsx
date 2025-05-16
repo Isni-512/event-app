235511aa01d5a7357c6115728c1c602ceddd6bc3
israelCN <2723520@collegelacite.ca>
2025-05-16 11:18:47 -0400

    page init

---
+++ app/page.jsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+export default function Home() {
+    return (
+            <div className="text-blue-700">
+                Soyez la bienvenue sur event-app
+            </div>
+    );
+}
