f5b996408422d8675f12074c36afb70eb2b2dbe2
israelCN <2723520@collegelacite.ca>
2025-05-22 22:48:17 -0400

    Ajustement de la présentation et du header

--- components/home/Presentation.jsx
+++ components/home/Presentation.jsx
@@ -3,11 +3,11 @@ import MarcheNoel from "@/public/img/MarcheNoel.webp";
 
 export default function Presentation() {
   return (
-    <section className="p-8 max-w-4xl mx-auto">
+    <section className="flex flex-col items-center p-8 max-w-6xl mx-auto">
       <h1 className="text-4xl font-bold text-center text-red-800 mb-6">
         Bienvenue au Marché de Noël🎄
       </h1>
-      <Image src={MarcheNoel} alt="Marché de Noël festif"  />
+      <Image src={MarcheNoel} alt="Marché de Noël festif" height={500}  className="p-8" />
 
       <p className="text-lg mb-4">
         Plongez dans la magie de Noël avec notre marché festif situé en plein cœur de la ville.
