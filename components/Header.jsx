24712dd3805ae312651914318f734ec1f13fdb42
EuniceMuleka <Eunice@gmail.com>
2025-05-22 22:35:12 -0400

    ChangementCssFooter

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,7 @@
 
 export default function Header() {
     return (
-        <header className="flex justify-center h-20 items-center bg-blue-700 text-white">
+        <header className="flex justify-center h-20 items-center bg-blue-900 text-white">
             Information pratique
         </header>
     );
