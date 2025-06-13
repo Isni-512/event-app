7a0a8177996bced4db2d53226019056586f03a27
israelCN <2723520@collegelacite.ca>
2025-06-13 06:16:21 -0400

    Remplacement par la balise Link

---
+++ app/jour3/page.jsx
@@ -0,0 +1,7 @@
+import Jour3 from "@/components/jour/Jour3";
+
+export default function Page() {
+    return <>
+        <Jour3 />
+    </>;
+}
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,27 +1,25 @@
-import Image from "next/image";
+import Link from "next/link";
 import NavBar from "./Nav";
 import { TbChristmasTreeFilled } from "react-icons/tb";
 
-function Logo({ changePage }) {
-    return (
-        <button onClick={() => changePage("Accueil")} className="cursor-pointer">
-            <div className="flex items-center gap-2">
-                <TbChristmasTreeFilled className="green-400" />
-                <h1 className="text-xl font-bold">
-                    Marché de Noël
-                </h1>
-            </div>
-        </button>
-    );
+function Logo() {
+  return (
+    <Link href="/" >
+      <div className="flex items-center gap-2">
+        <TbChristmasTreeFilled className="text-green-400 text-2xl" />
+        <h1 className="text-xl font-bold">Marché de Noël</h1>
+      </div>
+    </Link>
+  );
 }
 
-export default function Header({ changePage }) {
-    return (
-        <header className="bg-blue-900 text-white shadow-xl fixed top-0 left-0 w-full z-50">
-            <div className="max-w-8xl mx-auto px-4 flex justify-between items-center">
-                <Logo changePage={changePage} />
-                <NavBar setPage={changePage} />
-            </div>
-        </header>
-    );
+export default function Header() {
+  return (
+    <header className="bg-blue-900 text-white shadow-xl fixed top-0 left-0 w-full z-50">
+      <div className="max-w-8xl mx-auto px-4 flex justify-between items-center">
+        <Logo />
+        <NavBar />
+      </div>
+    </header>
+  );
 }
