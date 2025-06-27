0e2e38a2e92743b2ac90f8cf4bcc7998d0ce2d21
EuniceMuleka <Eunice@gmail.com>
2025-06-27 11:23:29 -0400

    Ajout des fichiers robots et sitemap

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,4 +1,13 @@
 import Contact from "@/components/Contact";
+export const metadata = {
+    title: 'Contacts',
+    description: 'Une question ou une demande ? Envoyez-nous un message grâce à ce formulaire de contact. Nous vous répondrons rapidement.',
+    openGraph: {
+      title: 'Contacts',
+      description: 'Une question ou une demande ? Envoyez-nous un message grâce à ce formulaire de contact. Nous vous répondrons rapidement.',
+      images: ['./img/icon_santa.webp']
+    }
+}
 
 export default function Page() {
     return <>
--- app/jour1/page.jsx
+++ app/jour1/page.jsx
@@ -1,4 +1,13 @@
 import Jour1 from "@/components/jour/Jour1";
+export const metadata = {
+    title: 'Jour1',
+    description: 'Découvrez le programme du 24 décembre : ouverture du marché, contes pour enfants, chorale de Noël et arrivée du Père Noël.',
+    openGraph: {
+      title: 'Jour1',
+      description: 'Découvrez le programme du 24 décembre : ouverture du marché, contes pour enfants, chorale de Noël et arrivée du Père Noël.',
+      images: ['./img/icon_santa.webp']
+    }
+}
 
 export default function Page() {
     return <>
